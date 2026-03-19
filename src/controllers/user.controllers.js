8f7ea22e978afe1953aeddf8db61f62feb843c68
Prachi-3101 <prachistudies22@gmail.com>
2026-03-19 12:05:47 +0530

    add user profile management features: change password, update account details, and avatar handling

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -194,8 +194,158 @@ const registerUser = asyncHandler( async(req,res)=>{
       }
     })
 
+     const changeCurrentPassword = asyncHandler(async(req, res) => {
+      const {oldPassword, newPassword} = req.body
+
+      const user = await User.findById(req.user?._id)
+      const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+
+      if(!isPasswordCorrect){
+         throw new ApiError(400, "Invalid old password")
+      }
+
+      user.password = newPassword
+      await user.save({validateBeforeSave: false})
+
+      return res
+      .status(200)
+      .json(new ApiResponse(200, {}, "Password changed successfully"))
+     })
+
+     const getCurrentUser = asyncHandler(async(req,res) =>{
+      return res
+      .status(200)
+      .json(new ApiResponse(
+         200,
+         req.user,
+         "User fetched successfully"
+      ))
+     })
+
+     const updateAccountDetails = asyncHandler(async(req,res) => {
+      const {fullName, email} = req.body
+
+      if(!fullName || !email){
+         throw new ApiError(400,"All fields are required")
+      }
+
+      const user = await User.findByIdAndUpdate(
+         req.user?._id,
+         {
+            $set: {
+               fullName: fullName,
+               email: email  
+            }
+         },
+         {new: true}
+      ).select("-password")
+
+      return res
+      .status(200)
+      .json(new ApiResponse(200,user,"Account details updated successfully"))
+     });
+
+     const updateAvatar = asyncHandler(async(req,res)=>{
+      const avatarLocalPath = req.file?.path
+
+      if(!avatarLocalPath){
+         throw new ApiError(400, "Avatar file is missing")
+      }
+
+     const deleteAvatar = asyncHandler(async(req,res)=>{
+      const user = await User.findByIdAndDelete(
+         req.user?._id,
+         {
+            $unset: {
+               avatar: avatar.url 
+            }
+         },  {new: true}
+      ).select("-password");
+
+      return res 
+      .status(200)
+      .json(
+         ApiResponse(200, "Avatar deleted")
+      )
+     })
+
+      const avatar = await uploadOnCloudinary(avatarLocalPath)
+
+      if(!avatar.url){
+         throw new ApiError(400, "Error while uploading avatar")
+      }
+
+      const user = await User.findByIdAndUpdate(
+         req.user?._id,
+         {
+            $set: {
+               avatar: avatar.url 
+            }
+         },
+         {new: true} 
+      ).select("-password")
+
+      return res
+      .status(200) 
+      .json(
+         new ApiResponse(200, user, "Avatar image updated successfully")
+      )
+     })
+
+     const updateUserCoverImage = asyncHandler(async(req,res)=>{
+      const coverImgLocalPath = req.file?.path 
+
+      if(!coverImgLocalPath){
+         throw new ApiError(400, "Cover image file is missing")
+      }
+
+      const deleteCoverImage = asyncHandler(async(req,res)=>{
+         const user = await User.findByIdAndDelete(
+            req.user?._id,
+            {
+               $unset: {
+                  coverImage: coverImage.url 
+               }
+            },
+            {new: true}
+         ).select("-password")
+      })
+
+      const coverImage = await uploadOnCloudinary(coverImgLocalPath)
+
+      if(!coverImage.url){
+         throw new ApiError(400, "Error while uploading on avatar")
+      }
+
+      const user = await User.findByIdAndUpdate(
+         req.user?._id,
+         {
+            $set: {
+               coverImage: coverImage.url 
+            }
+         },
+         {new: true} 
+      ).select("-password")
+
+      return res
+      .status(200)
+      .json(
+         new ApiResponse(200,user,"Cover image updated successfully")
+      )
+     })
+
    
 
     
 
-export {registerUser,loginUser,logoutUser,refreshAccessToken}
+export {registerUser,
+         loginUser,
+         logoutUser,
+         refreshAccessToken,
+         changeCurrentPassword,
+         getCurrentUser,
+         updateAccountDetails,
+         updateAvatar,
+         deleteAvatar,
+         updateUserCoverImage,
+         deleteCoverImage}
